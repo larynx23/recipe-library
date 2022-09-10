db0238b0f010620bddf3a1cc87e252a099a1f9a9
11Firefox11 <t.viktorvagyok@gmail.com>
2022-09-10 11:27:37 +0200

    Recept javascript tölti be a receptet név alapján

--- src/scripts/receipt.js
+++ src/scripts/receipt.js
@@ -1,4 +1,41 @@
+let ingredientsMultiplier = 0;
+const ingredientsLi = document.querySelector("#ingredients > ul").innerHTML;
+const stepsLi = document.querySelector("#steps > ol").innerHTML;
 
+function initView() {
+  document.title = `Recept - ${receipt['name']}`;
+  document.querySelector("#hero > h1").textContent = receipt['name'];
+  document.querySelector("#hero > img").src = `../../public/receipts/${receipt['image']}`;
+  for (let i = 0; i < 3; i++) {
+    document.querySelector(`#hero > table > tbody > tr:nth-child(2) > td:nth-child(${i+1})`).textContent = secsToText((i === 2) ? (receipt["times"][0] + receipt["times"][1]) : (receipt["times"][i]));
+  }
+  document.querySelector("body > main").classList.remove("loading");
+  const ingredientsUl = document.querySelector("#ingredients > ul");
+  ingredientsUl.innerHTML = "";
+  ingredientsMultiplier = 1;
+  receipt["ingredients"].forEach((ing, i) => {
+    ingredientsUl.innerHTML += ingredientsLi.replaceAll("{{ i }}", i).replaceAll("{{ count }}", ing.count).replaceAll("{{ unit }}", ing.unit);
+  });
+  document.querySelector("#steps > ol").innerHTML = "";
+  receipt["steps"].forEach((step) => {
+    document.querySelector("#steps > ol").innerHTML += stepsLi.replaceAll("{{ step }}", step);
+  });
+}
+
+function secsToText(seconds) {
+  console.log(seconds);
+  const timesMark = ["ó", "p", "mp"];
+  let times = [Math.floor(Math.floor(seconds / 60) / 60)];
+  seconds -= times[0] * 60 * 60;
+  times.push(Math.floor(seconds / 60), seconds % 60);
+  let toReturn = [];
+  times.forEach((t, i) => {
+    if (t > 0) {
+      toReturn.push(`${t}${timesMark[i]}`);
+    }
+  })
+  return toReturn.join(" ");
+}
 
 function selectStep(element) {
   const currSelected =document.querySelector("#steps > ol > li.selected");
@@ -7,3 +44,5 @@ function selectStep(element) {
   }
   element.classList.add("selected");
 }
+
+initView();
