24ff90247671b09077a183f74c390879852b5f61
11Firefox11 <t.viktorvagyok@gmail.com>
2022-09-10 10:22:03 +0200

    Recept adat felépítésének változtatása - hozzávalók object-é alakítása

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -8,7 +8,7 @@ id: string - Ez az object kulcsa, snake case recept neve
 name: string - Recept neve
 image: string - Recept képének a neve és kiterjesztése
 times: []number - Recept előkészítési és sütési ideje MÁSODPERCBEN
-ingredients: []string - Hozzávalók lista
+ingredients: []{count: number, unit: string} - Hozzávalók lista: count azt jelenti, hogy hány kell EGY elkészítéséhez és a unit pedig az ami ezután a szám után oda lesz írva
 steps: []string - Lépések lista
 */
 const receiptData = JSON.parse(`{
@@ -16,7 +16,15 @@ const receiptData = JSON.parse(`{
     "name": "Klasszikus madártej",
     "image": "klasszikus_madartej.jpeg",
     "times": [300, 360],
-    "ingredients": ["4 db tojásfehérje","1 csipet só","2 ek porcukor","800 ml tej","1 db vanília","4 db tojássárgája","70 g cukor","1 csipet só"],
+    "ingredients": [
+      {"count": 4, "unit": "db tojásfehérje"},
+      {"count": 1, "unit": "csipet só"},
+      {"count": 2, "unit": "ek porcukor"},
+      {"count": 800, "unit": "ml tej"},
+      {"count": 1, "unit": "db vanília"},
+      {"count": 4, "unit": "db tojássárgája"},
+      {"count": 70, "unit": "g cukor"}
+    ],
     "steps": [
       "A tojásfehérjét egy csipet sóval kezdjük el kemény habbá verni, majd adjuk hozzá a két evőkanál porcukrot, és verjük teljesen keményre.",
       "A tejhez adjuk hozzá a vaníliarúd kikapart magjait, majd forraljuk fel fel, és kanállal szaggassuk bele a tojáshabból galuskákat. Két-három percig főzzük, majd óvatosan for- dítsuk meg, újabb két-három perc, és már szedhetjük is ki egy üres tálba.",
