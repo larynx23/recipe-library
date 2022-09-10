e103c6f715dce09d07a2b9d7b6d4e195f7b82b50
11Firefox11 <t.viktorvagyok@gmail.com>
2022-09-10 09:54:02 +0200

    Recept url check receipt változó globálissá tétele

--- src/scripts/receipt_urlCheck.js
+++ src/scripts/receipt_urlCheck.js
@@ -1,8 +1,9 @@
 const receiptName = new URLSearchParams(window.location.search).get("name");
 const toMainPage = () => window.location.replace(window.location.origin + window.location.pathname.replace("receipt.html", "index.html"));
+let receipt;
 if (!receiptName) {
   toMainPage();
 } else {
-  const receipt = receiptData[receiptName];
+  receipt = receiptData[receiptName];
   if (!receipt) {toMainPage();}
 }
