fa5d07a6be6e46c18bd63143d929dba03ca473b4
11Firefox11 <t.viktorvagyok@gmail.com>
2022-09-17 19:49:31 +0200

    Téma színek navbar és recept oldalon főképp létrehozva

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,7 +1,11 @@
 const themButton = document.getElementById("theme-change");
 let darkMode = !window.localStorage.getItem("theme") ? window.matchMedia('(prefers-color-scheme: dark)').matches : window.localStorage.getItem("theme") === "dark";
+const root = document.querySelector(':root');
 if (!darkMode) {document.body.classList.add("light");}
 function toggleDarkMode(_, first) {
+  if (!first && getComputedStyle(root).getPropertyValue("--def-trans").endsWith("0s")) {
+    root.style.setProperty("--def-trans", ".25s");
+  }
   if (!first) {
     darkMode = !darkMode;
     window.localStorage.setItem("theme", darkMode ? "dark" : "light");
