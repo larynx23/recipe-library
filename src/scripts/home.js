a5033439802c184d828b81e06debaeb97b0158b2
chencsi <kevinkeeeh@gmail.com>
2022-09-26 13:30:59 +0200

    Szűrő dropdown elemei jsből renderelődnek

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,4 +1,5 @@
-const boxDiv = document.querySelector("html body main#receptek.container article.row.mt-3.mb-5").innerHTML;
+const boxDiv = document.querySelector("#receptek.container article.row.mt-3.mb-5").innerHTML;
+const filterDiv = document.querySelector("#receptek.container div.dropdown div.dropdown-menu").innerHTML;
 
 function init() {
     const section = document.querySelector('html body main#receptek.container article.row.mt-3.mb-5');
@@ -12,8 +13,22 @@ function init() {
             .replaceAll('{{ sum_times }}', secsToText(receipt.times[0] + receipt.times[1]))
             .replaceAll('{{ difficulty }}', difficultyText[receipt.difficulty])
             .replaceAll('{{ type }}', typeText[receipt.type])
-            .replaceAll('{{ money }}', moneyText[receipt.money])
+            .replaceAll('{{ money }}', moneyText[receipt.money]);
     }
 }
 
+function initFilter() {
+    const filterDropdown = document.querySelector("#receptek.container div.dropdown div.dropdown-menu");
+    filterDropdown.innerHTML = "";
+    for (const [num, display] of Object.entries(typeText)) {
+        console.log(filterDiv
+            .replaceAll("{{ type_num }}", num)
+            .replaceAll("{{ type }}", display), filterDiv);
+        filterDropdown.innerHTML += filterDiv
+            .replaceAll("{{ type_num }}", num)
+            .replaceAll("{{ type }}", display);
+    }    
+};
+
 init();
+initFilter();
