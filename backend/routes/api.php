25f672ec70549ef58ce2873da6e94a82ed42ad1a
Erdei Liliana <diak_346.erdei.liliana.maria@12.c.2023.com>
2025-02-13 19:19:26 +0100

    basic endpoints and routes

---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\RecipeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/recipes', [RecipeController::class, 'index']);
+Route::get('/recipes/{id}', [RecipeController::class, 'show']);
+Route::post('/recipes', [RecipeController::class, 'store']);
+Route::put('/recipes/{id}', [RecipeController::class, 'update']);
+Route::delete('/recipes/{id}', [RecipeController::class, 'destroy']);
