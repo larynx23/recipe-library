b585c815846eac76b0232a736636e9f525669f1b
larynx23 <hutyika64@gmail.com>
2025-02-15 19:00:59 +0100

    index.vue improved

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,64 +1,60 @@
 <template>
-<Layout>
-  <main>
-
-      <div class="mb-4">
-        <p class="pt-5 text-center">
+  <Layout>
+    <article class="flex flex-wrap">
+      <section class="w-full">
+        <p class="pt-5 text-center font-['Dancing_Script']">
           A jól kiválasztott recept bizony nem elég a sütemények készítéséhez. Legtöbbször felületesek a leírások, és
           alapvető lépéseket nem tartalmaznak.
           Minden esetben olvasd végig a receptet és számold meg a hozzávalók mennyiségét. Ha olyan sütit készítesz,
-          amelyet nyújtani kell, akkor számolj még pluszban liszttel a süti nyújtásához, illetve nagy valósínűséggel
+          amelyet nyújtani kell, akkor számolj még pluszban liszttel a süti nyújtásához, illetve nagy valószínűséggel
           szükséged lesz még plusz tojásra a tészta kenéséhez. Sok esetben ez nem derül ki a receptből.
         </p>
-
-        <div class="relative inline-block">
-          <button type="button" @click="dropDown" class="inline-flex rounded-md text-black border border-black p-3 bg-gray-400 text-sm hover:border-gray-500">
-          <img src="../../public/images/filter.svg" class="w-5 h-5 mr-2" alt="Filter" />Szűrő</button>
-
-          <div v-if="isOpen" class="absolute z-10 origin-top-right left-0 mt-2 rounded shadow-lg bg-gray-200 border border-black">
-            <div class="py-1">
-              <div
-                v-for="type in types" :key="type" class="dropdown-item px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
-                <label class="flex items-center text-black">
-                  <input type="checkbox" @change="filtered(type)" class="mr-2" />
-                  {{ type }}
-                </label>
-              </div>
+      </section>
+      <section class="w-full my-5">
+        <div class="dropdown flex justify-end">
+          <button type="button"
+            class="flex items-center px-4 py-2 border-2 border-white text-white hover:border-zinc-500 bg-black/10 transition-colors duration-200"
+            @click="toggleDropdown">
+            <img src="/images/filter.svg" class="w-[27px] mr-2" alt="Filter">
+            Szűrő
+          </button>
+          <div v-show="isDropdownOpen" class="z-10 absolute mt-12 bg-zinc-800 shadow-lg rounded-md overflow-hidden">
+            <div v-for="type in types" :key="type" class="hover:bg-zinc-600 transition-colors duration-200">
+              <label class="flex items-center px-4 py-2 text-white cursor-pointer w-full">
+                <input type="checkbox" @change="toggleFilter(type)" :id="`${type}_check`" class="w-4 h-4 mr-2">
+                {{ type }}
+              </label>
             </div>
+          </div>
         </div>
+      </section>
+    </article>
+    <div class="grid justify-items-center sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 relative">
+      <RecipeCard v-for="i in 5" />
     </div>
-
-      </div>
-
-    <div class="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 relative">
-    <RecipeCard v-for="i in 5" />
-    </div>
-  </main>
-</Layout>
+  </Layout>
 </template>
 
 <script>
-import Layout from '../layouts/Layout.vue';
-import RecipeCard from '../components/RecipeCard.vue';
+import Layout from '../layouts/Layout.vue'
+import RecipeCard from '../components/RecipeCard.vue'
 
 export default {
-  data() {
-    return {
-      isOpen: false,
-      types: ['eloetel', 'desszert', 'foetel'],
-    };
-  },
-  methods: {
-    dropDown() {
-      this.isOpen = !this.isOpen;
-    },
-    filtered(key){
-      console.log(key)
-    }
-  },
   components: {
     Layout,
     RecipeCard
+  },
+
+  data() {
+    return {
+      isDropdownOpen: false,
+    }
+  },
+
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen
+    },
   }
-};
+}
 </script>
