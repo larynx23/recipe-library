f01b1b2fc32b50343c57e948c89a7328c926fe34
larynx23 <hutyika64@gmail.com>
2025-02-17 17:18:02 +0100

    darkmode to footer added

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-200 flex justify-center items-center p-8">
+    <div class="mt-10 dark:bg-zinc-900 bg-zinc-100 dark:text-white flex justify-center items-center p-8">
       <p class="">Copyright &copy; 2022 All Rights Reserved by
         <a href="https://github.com/njit-Oregano/kedvenc-etelek" target="_blank">OREGANO</a>.
       </p>
