4cb32d7d52e5262d63dd2787ff4618c718cba415
larynx23 <hutyika64@gmail.com>
2025-02-17 17:16:07 +0100

    darkmode added to cards

--- frontend/src/components/RecipeCard.vue
+++ frontend/src/components/RecipeCard.vue
@@ -1,21 +1,19 @@
 <template>
-
-    <RouterLink class="bg-gray-400 flex flex-col justify-center items-center w-64 p-5 transform transition duration-300 hover:scale-105 ">
-      <img src="" class="w-20 h-20" />
-      <h3 class="text-2xl p-2">Etel nev</h3>
-      <p>leiras leiras recept leiras. nagyon finom.</p>
-      <hr class="w-full my-2 border-black" />
-      <div class="w-full">
-        <div class="flex justify-between">
-          <p>30p</p>
-          <p>konnyu</p>
-        </div>
-        <div class="flex justify-between">
-          <p>kaja</p>
-          <p>atlagos</p>
-        </div>
+  <RouterLink
+    class="bg-zinc-400 dark:bg-zinc-700 flex flex-col justify-center items-center w-64 p-5 transform transition duration-300 hover:scale-105">
+    <img src="" class="w-20 h-20" />
+    <h3 class="text-2xl p-2 text-black dark:text-white">Etel nev</h3>
+    <p class="text-black dark:text-zinc-200">leiras leiras recept leiras. nagyon finom.</p>
+    <hr class="w-full my-2 border-black dark:border-white" />
+    <div class="w-full text-black dark:text-white">
+      <div class="flex justify-between">
+        <p>30p</p>
+        <p>konnyu</p>
       </div>
-    </RouterLink>
-
-  </template>
-  
+      <div class="flex justify-between">
+        <p>kaja</p>
+        <p>atlagos</p>
+      </div>
+    </div>
+  </RouterLink>
+</template>
