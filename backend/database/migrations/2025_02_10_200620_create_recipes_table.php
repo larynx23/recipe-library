c215cd050791c05065c879fc7cbb07776b60088f
Erdei Liliana <diak_346.erdei.liliana.maria@12.c.2023.com>
2025-02-21 16:29:10 +0100

    database fix

--- backend/database/migrations/2025_02_10_200620_create_recipes_table.php
+++ backend/database/migrations/2025_02_10_200620_create_recipes_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type');
+            $table->integer('type');
             $table->integer('cost');
             $table->integer('difficulty');
             $table->text('description');
